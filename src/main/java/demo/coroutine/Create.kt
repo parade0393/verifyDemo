1b561767e854e22927e6590b59fa7391083f31fc
parade岁月 <13703937803@163.com>
2022-05-31 11:06:02 +0800

    创建协程和启动协程

--- src/main/java/demo/coroutine/Create.kt
+++ src/main/java/demo/coroutine/Create.kt
@@ -1,9 +1,6 @@
 package demo.coroutine
 
-import kotlin.coroutines.Continuation
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
-import kotlin.coroutines.createCoroutine
+import kotlin.coroutines.*
 
 fun main() {
     //创建协程
@@ -19,5 +16,16 @@ fun main() {
         }
 
     })
-   createCoroutine.resumeWith(Result.success(Unit))
+    //启动协程
+   createCoroutine.resume(Unit)
+    suspend {
+        "Coroutineff"
+    }.startCoroutine(object :Continuation<String>{
+        override val context: CoroutineContext = EmptyCoroutineContext
+
+        override fun resumeWith(result: Result<String>) {
+           println("创建及执行:${result}")//会立即输出
+        }
+
+    })
 }
