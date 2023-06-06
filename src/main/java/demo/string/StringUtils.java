d6c2ef1ec6c11c18c099dc14326bc6d03d74397c
parade岁月 <13703937803@163.com>
2023-06-06 18:11:56 +0800

    获取Url参数的方法

--- src/main/java/demo/string/StringUtils.java
+++ src/main/java/demo/string/StringUtils.java
@@ -1,11 +1,22 @@
 package demo.string;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringUtils {
     public static void main(String[] args) {
-        float dd = stripTrailingZero("dd");
-        System.out.println(dd);
+        String url = getParamByUrl("xgscheme://com.zwt.push/workplatform_web?type=102&id=79&url=http%3a%2f%2f218.29.175.190%3a8090%2fHtml%2foa%2f6447%2f5.2.3.68%2fcwb%2findex.html%23%2freceive-detail%3fid%3d79%26mobile%3d13703937803%26token%3d3d5fc44f824f4345a684a328571953bc%26govId%3d6447&areaKey=PY","url");
+//        try {
+//            String decode = URLDecoder.decode(url, "utf-8");
+//            System.out.println(decode);
+//        } catch (UnsupportedEncodingException e) {
+//            throw new RuntimeException(e);
+//        }
+        System.out.println(url);
     }
 
     /**
@@ -24,5 +35,27 @@ public class StringUtils {
 
     public static float stripTrailingZero(String str){
         return  new BigDecimal("6003.00010111").stripTrailingZeros().floatValue();
+    }
+
+    public static String getParamByUrl(String url, String name) {
+        url += "&";
+        String pattern = "(\\?|&){1}#{0,1}" + name + "=[^#&]*(&{1})";
+
+        Pattern r = Pattern.compile(pattern);
+
+        Matcher m = r.matcher(url);
+        if (m.find()) {
+            String replace = m.group(0).split("=")[1].replace("&", "");
+            try {
+                String decode = URLDecoder.decode(replace, StandardCharsets.UTF_8.toString());
+                return decode;
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                return "";
+            }
+
+        } else {
+            return null;
+        }
     }
 }
